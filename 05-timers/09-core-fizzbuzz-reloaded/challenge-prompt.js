a46cb645fad7aae7ef8ccd182b48de9f658b15fe
CB <byrne.christie@gmail.com>
2018-11-18 17:02:56 -0800

    code drill 1 ajax

--- 05-timers/09-core-fizzbuzz-reloaded/challenge-prompt.js
+++ 05-timers/09-core-fizzbuzz-reloaded/challenge-prompt.js
@@ -1,21 +1,27 @@
 // Wibble Wobble
 
-// This function checks whether the argument string fulfills the requirements
+// # Wibble Wobble
+
+// ## In this exercise, we're going to iterate through an array of strings. If the current element has an even number of characters, replace it with the string "Wibble". If the element has an odd number of vowels, replace it with the string "Wobble". If it has both replace it with the string "WibbleWobble"
+
+// * This activity is very similar to FizzBuzz, but because we're working with a different data type, the logic is more challenging.
+//     * For this exercise, only "a", "e", "i", "o", and "u" are considered vowels
+
+// * Only code in the indicated sections within the javascript file. You should not have to change the html or any code outside of yor designated areas.
+
+
+// // This function checks whether the argument string fulfills the requirements
 // for "Wibble". It should return true if the string has an even number of characters
 // and false if it doesn't
 function checkWibble(str) {
-
   // ---------- Your Code Here ----------
 
+  // check if the `str` has an even number of characters
+  // return true if it does and false if it doesn't
 
-
-
-
-
-
-
+  
+  
   // ----------- End Code Area -----------
-
 }
 
 
@@ -23,18 +29,33 @@ function checkWibble(str) {
 // for "Wobble". It should return true if the string has an odd number of vowels
 // and false if it doesn't
 function checkWobble(str) {
-
   // ---------- Your Code Here ----------
 
+  // create a string of the all vowels for us to check the letters against
+
+  
+
+  // declare a variable to track the number of vowels in the world and 
+  // instantiate it to 0
+
+  
+
+  // iterate through the characters in the `str`
+
+  
+
+    // if the currect letter is a vowel, increment the variable used to track the
+    // number of vowels
+
+    
+
+  // check if the number of vowels is odd
+  // return true if it is and false if it isn't
 
   
 
 
-
-
-
   // ----------- End Code Area -----------
-
 }
 
 
@@ -45,16 +66,33 @@ function checkWobble(str) {
 // "WibbleWobble". 
 
 function wibbleWobble(arr) {
-  
   // ---------- Your Code Here ----------
 
+  // iterate through the array
 
   
 
+    // check whether the current element passes the "Wibble" test
 
+    
 
+    // check whether the current element passes the "Wobble" test
 
+    
 
+    // if the element passed both tests, replace it with "WibbleWobble"
+
+    
+    
+    // if the element only passed the "Wibble" test, replace it with "Wibble"
+
+    
+
+    // if the element only passed the "Wobble" test, replace it with "Wobble"
+
+    
+    
+  
   // ----------- End Code Area -----------
   return arr;
 }
@@ -74,6 +112,7 @@ $(function() {
     "okay",
     "seeks",
     "desk",
+    "chair",
     "amazing",
     "wonder",
     "excel",
@@ -85,11 +124,11 @@ $(function() {
 
   // Make a copy of the testArray to use with the `wibbleWobble` function
   var newArray = wibbleWobble(testArray.slice(0));
-
+  
   // This populates our table with the initial values of the array
   // and what they are after our `wibbleWobble` function
   for (var i = 0; i < testArray.length; i++) {
-    var newRow = $("<tr>")
+    var newRow = $("<tr>");
     newRow.append($("<td>").text(testArray[i]));
     newRow.append($("<td>").text(newArray[i]));
     $("#display").append(newRow);
